552fff3c5f16a6c749dab539dff5adb2311f6efc
FrancoRutigliano <francorutigliano11@gmail.com>
2023-10-17 20:46:00 -0300

    refactored code and routes

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"context"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/FrancoRutiliano/orders-api/aplication"
 )
 
 func main() {
+	app := aplication.New()
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
+	err := app.Start(context.TODO())
 
-	router.Get("/hello", basicHandler)
-	// Obteniendo informacion de los accesos en cada peticion
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-
-	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Print("Fallo en listar y servir el server ", err)
+
 	}
 
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello Wordl"))
 }
